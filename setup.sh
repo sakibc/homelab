7658284304d2aa354bc5469e6e7053894bd4159b
Sakib Chowdhury <sakib_c@outlook.com>
2023-05-14 15:40:08 -0400

    more homelab

--- setup.sh
+++ setup.sh
@@ -5,9 +5,32 @@ abort_setup () {
   exit 1
 }
 
+# Check that python3 is installed
+if ! command -v python3 &> /dev/null; then
+  echo "python3 not found"
+  abort_setup
+fi
+
+# Ensure pip is installed
+echo "Ensuring pip is installed..."
+python3 -m ensurepip --upgrade || abort_setup
+
+# Check if ansible module is installed and install if not
+if ! python3 -c "import ansible" &> /dev/null; then
+  echo "ansible module not found, installing..."
+  pip3 install ansible || abort_setup
+fi
+
+echo
+
+# Interactively update keys in known_hosts file if host is new or they have changed
+current_dir=`pwd`
+cd setup
 ./keyscan.sh || abort_setup
+cd $current_dir
 
 echo
 echo "Starting Ansible"
 
-ansible-playbook -i homelab.yml site.yml
+# Run ansible playbook with args passed to this script
+ansible-playbook -i setup/homelab.yml setup/site.yml $@ || abort_setup
